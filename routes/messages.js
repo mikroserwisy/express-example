f7fbf5fb334a3e67be31c55ceef6561343f84e5f
landrzejewski <landrzejewski.poczta@gmail..com>
2021-08-06 09:49:06 +0200

    base server

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,5 @@
 const messages = require('../data/messages.json');
-const { notFoundHandler } = require('../utils');
+const { notFoundHandler, parseBody, generateId } = require('../utils');
 const express = require("express");
 
 const getMessages = (request, response) => {
@@ -15,13 +15,50 @@ const getMessage = (request, response) => {
     }
 };
 
+const addMessage = async (request, response) => {
+    const body = await parseBody(request);
+    const message = { ...JSON.parse(body), id: generateId() }
+    messages.push(message);
+    response.status(201);
+    response.setHeader('Location', `/message/${message.id}`);
+    response.end();
+};
+
+const updateMessage = async (request, response) => {
+    const body = await parseBody(request);
+    const message = messages.find(message => message.id === request.params.id);
+    if (!message) {
+        notFoundHandler(request, response)
+    } else {
+        Object.assign(message, JSON.parse(body));
+        response.send(message);
+    }
+};
+
+const deleteMessage = async (request, response) => {
+    const index = messages.findIndex(message => message.id === request.params.id);
+    if (index === -1) {
+        notFoundHandler(request, response)
+    } else {
+        messages.splice(index, 1);
+        response.sendStatus(204);
+    }
+};
+
 const getMessagesForUsers = (request, response) => {
     response.send(request.params);
 };
 
 const router = express.Router();
-router.get('/', getMessages);
-router.get('/:id', getMessage);
+router.route('/')
+    .get(getMessages)
+    .post(addMessage);
+
+router.route('/:id')
+    .get(getMessage)
+    .patch(updateMessage)
+    .delete(deleteMessage);
+
 router.get('/from/:sender/to/:recipient', getMessagesForUsers);
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -1,9 +1,24 @@
+const { request } = require("express");
+const crypto = require('crypto');
+
 const notFoundHandler = (request, response) => {
     const route = `${request.method} ${request.url}`;
     response.statusCode = 404;
     response.end(`Route ${route} not found`);
 };
 
+const parseBody = (request) => {
+  return new Promise(resolve => {
+      const chunks = [];
+      request.on('data', (chunk) => chunks.push(chunk));
+      request.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+};
+
+const generateId = () => crypto.randomBytes(8).toString('hex');
+
 module.exports = {
-    notFoundHandler
+    notFoundHandler,
+    parseBody,
+    generateId
 };
