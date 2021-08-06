fa53a1c83ed6b25d07beb5112eb89aa925b66cba
landrzejewski <landrzejewski.poczta@gmail..com>
2021-08-06 08:38:56 +0200

    base server

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-//const http = require('http');
 const express = require('express');
 const messages = require('./data/messages.json');
 const users = require('./data/users.json');
 
+const getMessages = (request, response) => {
+    response.send(messages);
+};
+
+const getUsers = (request, response) => {
+    response.send(users);
+};
+
+const defaultHandler = (request, response) => {
+    const route = `${request.method} ${request.url}`;
+    response.statusCode = 404;
+    response.end(`Route ${route} not found`);
+};
+
+const routes = {
+    'GET /messages': getMessages,
+    'GET /users': getUsers
+}
+
 const app = express();
 app.use((request, response) => {
     const route = `${request.method} ${request.url}`;
+    const handler = routes[route] || defaultHandler;
     console.log(route);
-    if (route === 'GET /messages') {
-        response.send(messages);
-    } else if (route === 'GET /users') {
-        response.send(users);
-    } else {
-        response.statusCode = 404;
-        response.end(`Route ${route} not found`);
-    }
+    handler(request, response);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
-
-//const server = http.createServer(app);
-//server.listen(3000, () => console.log('Listening on port 3000'));
