dc422579e819c41f94f50e27aebd676c686c99f5
landrzejewski <landrzejewski.poczta@gmail..com>
2021-08-06 08:32:02 +0200

    base server

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const http = require('http');
+//const http = require('http');
+const express = require('express');
 const messages = require('./data/messages.json');
 const users = require('./data/users.json');
 
-const server = http.createServer((request, response) => {
+const app = express();
+app.use((request, response) => {
     const route = `${request.method} ${request.url}`;
     console.log(route);
-    response.setHeader('Content-Type', 'application/json');
     if (route === 'GET /messages') {
-        response.end(JSON.stringify(messages));
+        response.send(messages);
     } else if (route === 'GET /users') {
-        response.end(JSON.stringify(users));
+        response.send(users);
     } else {
         response.statusCode = 404;
         response.end(`Route ${route} not found`);
     }
 });
 
-server.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+//const server = http.createServer(app);
+//server.listen(3000, () => console.log('Listening on port 3000'));
