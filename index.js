3d51a9a04983a1d07ad0291620fa54b3ed4578a9
landrzejewski <landrzejewski.poczta@gmail..com>
2021-08-06 08:53:38 +0200

    base server

--- index.js
+++ index.js
@@ -6,27 +6,46 @@ const getMessages = (request, response) => {
     response.send(messages);
 };
 
+const getMessage = (request, response) => {
+   const message = messages.find(message => message.id === request.params.id);
+   if (!message) {
+       notFoundHandler(request, response)
+   } else {
+       response.send(message);
+   }
+};
+
 const getUsers = (request, response) => {
     response.send(users);
 };
 
-const defaultHandler = (request, response) => {
+const getUser = (request, response) => {
+    const user = users.find(user => user.id === request.params.id);
+    if (!user) {
+        notFoundHandler(request, response)
+    } else {
+        response.send(user);
+    }
+};
+
+const getMessagesForUsers = (request, response) => {
+    response.send(request.params);
+};
+
+const notFoundHandler = (request, response) => {
     const route = `${request.method} ${request.url}`;
     response.statusCode = 404;
     response.end(`Route ${route} not found`);
 };
 
-const routes = {
-    'GET /messages': getMessages,
-    'GET /users': getUsers
-}
+const router = express.Router();
+router.get('/messages', getMessages);
+router.get('/messages/:id', getMessage);
+router.get('/messages/from/:sender/to/:recipient', getMessagesForUsers);
+router.get('/users', getUsers);
+router.get('/users/:id', getUser);
 
 const app = express();
-app.use((request, response) => {
-    const route = `${request.method} ${request.url}`;
-    const handler = routes[route] || defaultHandler;
-    console.log(route);
-    handler(request, response);
-});
+app.use(router);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
