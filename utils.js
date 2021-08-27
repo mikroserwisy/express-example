3481a512e29a5d0c1ae4841b2fffbc491e8bc5bf
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-08-27 02:43:54 +0200

    fix

--- utils.js
+++ utils.js
@@ -8,28 +8,9 @@ const notFoundHandler = (request, response) => {
     response.end(`Route ${route} not found`);
 };
 
-/*const parseBody = (request) => {
-  return new Promise(resolve => {
-      const chunks = [];
-      request.on('data', (chunk) => chunks.push(chunk));
-      request.on('end', () => resolve(Buffer.concat(chunks)));
-  });
-};
-
-const parseJson = async (request, response, next) => {
-    const body = await parseBody(request);
-    request.body = JSON.parse(body);
-    next();
-};*/
-
 const parseJson = bodyParser.json({limit: '150kb'});
 
 const generateId = () => crypto.randomBytes(8).toString('hex');
-
-/*const logger = (request, response, next) => {
-    console.log(`${request.method} ${request.url}`);
-    next();
-};*/
 
 const logger = morgan('tiny');
 
